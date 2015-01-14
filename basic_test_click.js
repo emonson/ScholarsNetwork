0f8ca235aba03b1b88bd8e001ed8eaaa63099b89
Eric E Monson <emonson@cs.duke.edu>
2015-01-14 10:51:24 -0500

    using newer data and trying to get csv to work

--- basic_test_click.js
+++ basic_test_click.js
@@ -24,101 +24,110 @@ var svg = d3.select("body").append("svg")
 //Read the data from a json file
 var graph = {};
 
-$.ajax({
-    url:'/data/authorsim_diffusion_nodes.json',
-    async:false,
-    dataType:'json',
-    success:function(data) {
-        // node.fixed being read in as a string rather than a boolean
-        // also, scale positions to pixels so dragging isn't scaled, too
-        for(var ii=0; ii < data.length; ii++) {
-            if (data[ii].fixed == "true" || data[ii].fixed === true) {
-                data[ii].fixed = true;
-            } else {
-                data[ii].fixed = false;
-            }
-            data[ii].x = x_scale(data[ii].x);
-            data[ii].y = y_scale(data[ii].y);
-        }
-        graph.nodes = data;
-    }
-});	
-$.ajax({
-    url:'/data/authorsim_diffusion_edges.json',
-    async:false,
-    dataType:'json',
-    success:function(data) {
-        graph.links = data;
-    }
-});	
-
-//Toggle stores whether the highlighting is on
-var toggle = 0;
-//Create an array logging what is connected to what
-var linkedByIndex = {};
-for (i = 0; i < graph.nodes.length; i++) {
-    linkedByIndex[i + "," + i] = 1;
+// Helper function for parsing nodes (authors departments positions) CSV
+var depts_accessor = function(d) { 
+    // "unary plus" is the fastest way to change from a string to a number
+    d.id = +d.id;
+    d.dept_id = +d.dept_id;
+    d.mod_class = +d.mod_class;
+    d.x = +d.x;
+    d.y = +d.y;
+    // keep nodes from moving in d3 layout
+    d.fixed = true;
+    return d;
 };
-graph.links.forEach(function (d) {
-    linkedByIndex[d.source.index + "," + d.target.index] = 1;
-});
-//This function looks up whether a pair are neighbours
-function neighboring(a, b) {
-    return linkedByIndex[a.index + "," + b.index];
-}
-function connectedNodes() {
-    if (toggle == 0) {
-        //Reduce the opacity of all but the neighbouring nodes
-        d = d3.select(this).node().__data__;
-        node.style("opacity", function (o) {
-            return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1;
-        });
-        //Reduce the op
-        toggle = 1;
-    } else {
-        //Put them back to opacity=1
-        node.style("opacity", 1);
-        toggle = 0;
-    }
-}
 
-// Testing click link filter
-function neighbor_links() {
-}
+// Helper function for parsing nodes (authors departments positions) CSV
+var edges_accessor = function(d) { 
+    // "unary plus" is the fastest way to change from a string to a number
+    d.edge_id = +d.edge_id;
+    d.source = +d.source;
+    d.target = +d.target;
+    d.weight = +d.weight;
+    return d;
+};
+
+// Use d3 for parsing csv directly instead of having to change it into json
+d3.csv('data/fa2_p0_k50a30.csv', depts_accessor, load_edges);
+
+var load_edges = function(node_data) {
+    graph.nodes = node_data;
+    d3.csv('data/authorsim_p0_k50a30_diffusion_edges.csv', edges_accessor, init_vis);
+};
+
+var init_vis = function(edge_data) {
     
-// Creates the graph data structure out of the json data
-force.nodes(graph.nodes)
-    .links(graph.links)
-    .start();
+    graph.links = edge_data;
+    
+    //Toggle stores whether the highlighting is on
+    var toggle = 0;
+    //Create an array logging what is connected to what
+    var linkedByIndex = {};
+    for (i = 0; i < graph.nodes.length; i++) {
+        linkedByIndex[i + "," + i] = 1;
+    };
+    graph.links.forEach(function (d) {
+        linkedByIndex[d.source.index + "," + d.target.index] = 1;
+    });
+    //This function looks up whether a pair are neighbours
+    function neighboring(a, b) {
+        return linkedByIndex[a.index + "," + b.index];
+    }
+    function connectedNodes() {
+        if (toggle == 0) {
+            //Reduce the opacity of all but the neighbouring nodes
+            d = d3.select(this).node().__data__;
+            node.style("opacity", function (o) {
+                return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1;
+            });
+            //Reduce the op
+            toggle = 1;
+        } else {
+            //Put them back to opacity=1
+            node.style("opacity", 1);
+            toggle = 0;
+        }
+    }
 
-// Create all the line svgs but without locations yet
-var link = svg.selectAll(".link")
-        .data(graph.links)
-    .enter();
-//         .append("line")
-//         .attr("class", "link");
-//     // .style("stroke-width", function (d) { return Math.sqrt(d.value); });
+    // Testing click link filter
+    function neighbor_links() {
+    }
+    
+    // Creates the graph data structure out of the json data
+    force.nodes(graph.nodes)
+        .links(graph.links)
+        .start();
 
-//Do the same with the circles for the nodes - no 
-var node = svg.selectAll(".node")
-        .data(graph.nodes)
-    .enter()
-        .append("circle")
-        .attr("class", "node")
-        .attr("r", function(d) { return r_scale(d.size); })
-        .style("fill", function (d) { return color(d.mod_class); })
-        .call(force.drag)
-        .on('click', connectedNodes);
+    // Create all the line svgs but without locations yet
+    var link = svg.selectAll(".link")
+            .data(graph.links)
+        .enter();
+    //         .append("line")
+    //         .attr("class", "link");
+    //     // .style("stroke-width", function (d) { return Math.sqrt(d.value); });
+
+    //Do the same with the circles for the nodes - no 
+    var node = svg.selectAll(".node")
+            .data(graph.nodes)
+        .enter()
+            .append("circle")
+            .attr("class", "node")
+            .attr("r", function(d) { return r_scale(d.size); })
+            .style("fill", function (d) { return color(d.mod_class); })
+            .call(force.drag)
+            .on('click', connectedNodes);
 
 
-//Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
-force.on("tick", function () {
-//     link.attr("x1", function (d) { return d.source.x; })
-//         .attr("y1", function (d) { return d.source.y; })
-//         .attr("x2", function (d) { return d.target.x; })
-//         .attr("y2", function (d) { return d.target.y; });
-// 
-    node.attr("cx", function (d) { return d.x; })
-         .attr("cy", function (d) { return d.y; });
-});
+    //Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
+    force.on("tick", function () {
+    //     link.attr("x1", function (d) { return d.source.x; })
+    //         .attr("y1", function (d) { return d.source.y; })
+    //         .attr("x2", function (d) { return d.target.x; })
+    //         .attr("y2", function (d) { return d.target.y; });
+    // 
+        node.attr("cx", function (d) { return d.x; })
+             .attr("cy", function (d) { return d.y; });
+    });
+
+};  // init_vis()
 
