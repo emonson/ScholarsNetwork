a719498312edb7f9306ae8a936b41619f5c4652b
Eric E Monson <emonson@cs.duke.edu>
2015-01-27 15:06:20 -0500

    info panel working. still need sorting on neighbor results

--- basic_test_click.js
+++ basic_test_click.js
@@ -1,7 +1,15 @@
-// http://www.coppelia.io/2014/07/an-a-to-z-of-extra-features-for-the-d3-force-layout/
+// Many pieces taken from examples in
+//   http://www.coppelia.io/2014/07/an-a-to-z-of-extra-features-for-the-d3-force-layout/
+// d3 tooltips
+//   https://github.com/Caged/d3-tip
+// Zoom to bounds
+//   http://bl.ocks.org/mbostock/9656675
+
+// -------------------------
+// Variable initialization
 
 //Constants for the SVG
-var width = 900,
+var width = 700,
     height = 600,
     active = d3.select(null);
 
@@ -10,7 +18,24 @@ var x_scale = d3.scale.linear().range([0, width]);
 var y_scale = d3.scale.linear().range([0, height]);
 var color = d3.scale.category20b().domain([1, 72]);
     
-//Set up the force layout
+// Data structure that d3 will use for "layout"
+var graph = {};
+
+// Convenience data structures for displaying edges, etc, on interaction
+var node_edges;
+var node_neighbors;
+// Separate list of just neighbor indices for testing if an ID is in the list of neighbors
+var node_neighbor_ids;
+
+// Array for search box autocomplete to use
+var author_names = [];
+
+// Not extremely memory-efficient, but keeping a lookup table of name -> ID for select after search
+var author_name_id_dict = {};
+
+// -------------------------
+// Set up the force layout
+
 var force = d3.layout.force()
     .charge(-120)
     .linkDistance(30)
@@ -51,14 +76,8 @@ svg_base
             reset();
         });
 
-//Read the data from a json file
-var graph = {};
-var node_neighbors;
-var node_edges;
-// Array for search box autocomplete to use
-var author_names = [];
-// Not extremely memory-efficient, but keeping a lookup table of name -> ID for select after search
-var author_name_id_dict = {};
+// -------------------------
+// Data reading
 
 // Helper function for parsing nodes (authors departments positions) CSV
 var depts_accessor = function(d) { 
@@ -85,10 +104,11 @@ var edges_accessor = function(d) {
 
 // Use d3 for parsing csv directly instead of having to change it into json
 var load_nodes = function() {
-    d3.csv('data/fa2_p0_k50a30.csv', depts_accessor, load_edges);
-    // d3.csv('data/fa2_usam_p0_k30a15.csv', depts_accessor, load_edges);
+    // d3.csv('data/fa2_p0_k50a30.csv', depts_accessor, load_edges);
+    d3.csv('data/fa2_usam_p0_k30a15.csv', depts_accessor, load_edges);
 };
 
+// This gets called after CSV node data is loaded (asynchronously)
 var load_edges = function(node_data) {
     
     // Pre-scale all xy node positions so dragging works properly
@@ -113,10 +133,11 @@ var load_edges = function(node_data) {
     author_names = author_names.sort();
     
     // Actually start loading edge data
-    d3.csv('data/authorsim_p0_k50a30_diffusion_edges.csv', edges_accessor, init_vis);
-    // d3.csv('data/usam_p0_k30a15_diffusion_edges.csv', edges_accessor, init_vis);
+    // d3.csv('data/authorsim_p0_k50a30_diffusion_edges.csv', edges_accessor, init_vis);
+    d3.csv('data/usam_p0_k30a15_diffusion_edges.csv', edges_accessor, init_vis);
 };
 
+// This gets called after CSV edge data is loaded (asynchronously)
 var init_vis = function(edge_data) {
     
     graph.links = edge_data;
@@ -129,16 +150,22 @@ var init_vis = function(edge_data) {
     // Create data structure to hold references to neighboring nodes and links
     var n_nodes = graph.nodes.length;
     node_neighbors = new Array(n_nodes);
+    node_neighbor_ids = new Array(n_nodes);
     node_edges = new Array(n_nodes);
     for (var ii=0; ii<n_nodes; ii++) {
         // include self id as neighbor for click highlighting
-        node_neighbors[ii] = [ii];
+        node_neighbor_ids[ii] = [ii];
+        // but for now don't include self in neighbors list that'll get used in info panel
+        node_neighbors[ii] = [];
         node_edges[ii] = [];
     }
     graph.links.forEach(function(d,i){
-        // node neighbors are IDs
-        node_neighbors[d.source.id].push(d.target.id);
-        node_neighbors[d.target.id].push(d.source.id);
+        // node neighbor IDs are just IDs
+        node_neighbor_ids[d.source.id].push(d.target.id);
+        node_neighbor_ids[d.target.id].push(d.source.id);
+        // node neighbors are node objects
+        node_neighbors[d.source.id].push(d.target);
+        node_neighbors[d.target.id].push(d.source);
         // edge neighbors are edge objects
         node_edges[d.source.id].push(d);
         node_edges[d.target.id].push(d);
@@ -151,10 +178,10 @@ var init_vis = function(edge_data) {
         });
     });
     
-    var link = svg.selectAll(".link");
+    var links = svg.selectAll(".link");
     
     // Circles for the NODES
-    var node = svg.selectAll(".node")
+    var nodes = svg.selectAll(".node")
             .data(graph.nodes, function(d){ return d.id; })
         .enter()
             .append("circle")
@@ -174,22 +201,27 @@ var init_vis = function(edge_data) {
     // the force layout is generating the co-ordinates 
     // which this code is using to update the attributes of the SVG elements
     force.on("tick", function () {
-        link.attr("x1", function (d) { return d.source.x; })
+        links.attr("x1", function (d) { return d.source.x; })
             .attr("y1", function (d) { return d.source.y; })
             .attr("x2", function (d) { return d.target.x; })
             .attr("y2", function (d) { return d.target.y; });
-        node.attr("cx", function (d) { return d.x; })
+        nodes.attr("cx", function (d) { return d.x; })
             .attr("cy", function (d) { return d.y; });
     });
 
 
-};  // init_vis()
+};  // close init_vis()
 
+
+// --------------------------
 // Start data loading 
+
 load_nodes();
 
-// Set up tooltip
-// https://github.com/Caged/d3-tip
+
+// --------------------------
+// Tooltips (hover)
+
 var tip = d3.tip()
     .attr('class', 'd3-tip')
     .offset([-8, 0])
@@ -206,6 +238,21 @@ function hover_out(d) {
     tip.hide(d);
 }
 
+    // TODO: hover also searches for ID in info_panel and highlights
+function info_hover_in(d) {
+    d3.select("#in_" + d.id).classed("hovered", true);
+    d3.select("#n_" + d.id).classed("hovered", true);
+}
+
+function info_hover_out(d) {
+    d3.select("#in_" + d.id).classed("hovered", false);
+    d3.select("#n_" + d.id).classed("hovered", false);
+}
+    
+
+// --------------------------
+// Diplay Edges on mousedown
+
 function display_connected_edges(D) { 
     
     // background svg group clears links on mousedown, so want to stop event from
@@ -215,10 +262,10 @@ function display_connected_edges(D) {
     }
     
     // Create only line svgs that are connected to the current node
-    link = svg_links.selectAll(".link")
+    var links = svg_links.selectAll(".link")
             .data(node_edges[D.id], function(d){return d.edge_id;});
     
-    link.enter()
+    links.enter()
         .append("line")
         .attr("class", "link")
         .attr("x1", function (d) { return d.source.x; })
@@ -227,16 +274,70 @@ function display_connected_edges(D) {
         .attr("y2", function (d) { return d.target.y; })
         .style("stroke-width", function (d) { return 1.5*Math.sqrt(d.weight) + 0.25; });
 
-    link.exit()
+    links.exit()
         .remove();
     
     // Test if nodes are in list of neighbor IDs, and if not, dim
+    // and clear all hovered
     svg.selectAll('.node')
-        .classed('dimmed', function(d,i){ return node_neighbors[D.id].indexOf(i) < 0; });
+        .classed('dimmed', function(d,i){ return node_neighbor_ids[D.id].indexOf(i) < 0; })
+        .classed('hovered', false);
     
     // TODO: sort un-dimmed nodes to top
+  
+    // Update info panel
+    update_info_panel(D);
 }
+
+// --------------------------
+// Info panel
+
+function update_info_panel(D) {
+    d3.selectAll(".info_container").remove();
     
+    var info_panel = d3.select("#info_panel")
+            .append("div")
+                .attr("class", "info_container");
+    
+    // Author name
+    info_panel.append("div")
+                .attr("class", "info_header")
+                .html(D.author);
+    // Degree
+    info_panel.append("div")
+                .attr("class", "info_data_subheader")
+                .html("neighbors " + node_neighbors[D.id].length);
+    
+    // Connections list
+    info_panel.append("div")
+                .attr("class", "info_title")
+                .html("Connections");
+    
+    info_panel.selectAll(".info_data_element")
+            .data(node_neighbors[D.id], function(d){ return d ? d.id : 0;})
+        .enter()
+            .append("div")
+                .attr("class", "info_data_element")
+                .on('mousedown', info_mousedown)
+                .on('mouseover', info_hover_in)
+                .on('mouseout', info_hover_out)
+            .append("div")
+                .attr("class", "info_data_element_contents")
+                .text(function(d){ return d.author; })
+            .append("div")
+                .attr("class", "info_data_element_circle")
+                .attr("id", function(d){ return "in_" + d.id; })
+                .style("background-color", function(d){return color(d.dept_id);});
+        
+    // TODO: sort by close connection (or weight)
+    // TODO: hover highlight in graph
+    // TODO: click select that name
+    // TODO: take out self-connection
+}   
+    
+// --------------------------
+// Zoom to bounds
+
 function clicked(d) {
   if (active.node() === this) return reset();
   active.classed("active", false);
@@ -258,34 +359,8 @@ function clicked(d) {
       .call(zoom.translate(translate).scale(scale).event);
 }
 
-function reset() {
-  active.classed("active", false);
-  active = d3.select(null);
-  
-  // clear out search box
-  document.getElementById('search').value = "";
-
-  svg_base.transition()
-      .duration(750)
-      .call(zoom.translate([0, 0]).scale(1).event);
-}
-
-function zoomed() {
-    svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-}
-
-function searchNode() {
-    //find the node
-    var selectedVal = document.getElementById('search').value;
-    if (selectedVal != "none") {
-        var node_id = author_name_id_dict[selectedVal];
-        // simulate both clicking and mousedown for the searched-for node
-        svg.select("#n_" + node_id).each(clicked).each(display_connected_edges);
-    }
-}
-
 function calculate_neighbor_bounds(d) {
-    var nn = node_neighbors[d.id];
+    var nn = node_neighbor_ids[d.id];
     var nodes = graph.nodes;
     var x_min=Infinity, y_min=Infinity, x_max=-Infinity, y_max=-Infinity;
     for (var ii=0; ii<nn.length; ii++) {
@@ -297,4 +372,44 @@ function calculate_neighbor_bounds(d) {
         y_max = y > y_max ? y : y_max;
     }
     return [[x_min, y_min], [x_max, y_max]]
+}
+
+function zoomed() {
+    svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+}
+
+// --------------------------
+// Reset on double-click
+
+function reset() {
+  active.classed("active", false);
+  active = d3.select(null);
+  
+  // clear out search box
+  document.getElementById('search').value = "";
+    
+  // clear out info panel
+  d3.selectAll(".info_container").remove();
+  
+  svg_base.transition()
+      .duration(750)
+      .call(zoom.translate([0, 0]).scale(1).event);
+}
+
+// --------------------------
+// Search
+
+function searchNode() {
+    //find the node
+    var selectedVal = document.getElementById('search').value;
+    if (selectedVal != "none") {
+        var node_id = author_name_id_dict[selectedVal];
+        // simulate both clicking and mousedown for the searched-for node
+        svg.select("#n_" + node_id).each(clicked).each(display_connected_edges);
+    }
+}
+
+function info_mousedown(d) {
+    //find the node
+    svg.select("#n_" + d.id).each(clicked).each(display_connected_edges);
 }
